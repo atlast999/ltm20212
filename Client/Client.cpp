7fb3945b69d81cdc8f9ce7e1bc9ef840a9061c6c
Thanh Do <dothanhqt9x@gmail.com>
2022-07-23 16:27:17 +0700

    Add login, register and getlist

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -7,8 +7,12 @@
 
 #include <winsock2.h>
 #include <ws2tcpip.h> //inet_pton
+#include <list>
 
 #include "util.h"
+
+#include "model.h";
+#include "constant.h";
 
 #define BUFFER_SIZE 2048
 #define gets_s(x, len) fgets((x), (len) + 1, stdin)
@@ -122,14 +126,14 @@ int main(int argc, char* argv[])
 *  buffer - request content to send to server
 * return response code
 */
-int startComunicatingWithServer(SOCKET& client, const char* buffer)
+string startComunicatingWithServer(SOCKET& client, const char* buffer)
 {
 	int ret, messageLen;
 	ret = send(client, buffer, strlen(buffer), 0);
 	if (ret == SOCKET_ERROR)
 	{
 		printf("Cannot send to server: %d\n", WSAGetLastError());
-		return -1;
+		return "-1";
 	}
 
 	// Receive response from server
@@ -138,10 +142,10 @@ int startComunicatingWithServer(SOCKET& client, const char* buffer)
 	if (ret == SOCKET_ERROR)
 	{
 		printf("Cannot recieve from server: %d", WSAGetLastError());
-		return -1;
+		return "-1";
 	}
 	response[ret] = 0;
-	return atoi(response);
+	return response;
 }
 
 /*
@@ -155,7 +159,31 @@ string getUserInput(const char* message)
 	cout << message;
 	string input;
 	getline(cin, input);
-	return input + "\r\n";
+	return input;
+}
+
+/*
+* Show register UI
+* param:
+*  client[IN] - socket param to pass to startComunicatingWithServer function
+* return feature code
+*/
+string showRegisterFeature(SOCKET& client)
+{
+	string username = getUserInput("Enter your username: ");
+	string password = getUserInput("Enter your password: ");
+
+	SignUpRequest request(username, password);
+	string rawRequest = request.serialize();
+	string response = startComunicatingWithServer(client, rawRequest.c_str());
+	SignUpResponse signUpResponse;
+	signUpResponse.deserialize(response);
+	if(signUpResponse.message == MESSAGE_NAME_INVALID){
+		return MESSAGE_NAME_INVALID;
+	} else if (signUpResponse.message == MESSAGE_SUCCESS)
+	{
+		return MESSAGE_SUCCESS;
+	}
 }
 
 /*
@@ -164,36 +192,54 @@ string getUserInput(const char* message)
 *  client[IN] - socket param to pass to startComunicatingWithServer function
 * return feature code
 */
-int showLoginFeature(SOCKET& client)
+string showLoginFeature(SOCKET& client)
 {
-	string input = getUserInput("Enter your username: ");
-	vector<string> requestParts = { CMD_IN, input };
-	string request = joinToString(requestParts);
-	int responseCode = startComunicatingWithServer(client, request.c_str());
-	handleResponseCode(responseCode);
-	return 1;
+	string username = getUserInput("Enter your username: ");
+	string password = getUserInput("Enter your password: ");
+	
+	LogInRequest request(username, password);
+	string rawRequest = request.serialize();
+	string response = startComunicatingWithServer(client, rawRequest.c_str());
+	LogInResponse logInResponse;
+	logInResponse.deserialize(response);
+	if(logInResponse.message == MESSAGE_NAME_INVALID){
+		return MESSAGE_NAME_INVALID;
+	} else if (logInResponse.message == MESSAGE_CREDENTIAL_INVALID)
+	{
+		return MESSAGE_CREDENTIAL_INVALID;
+	} else if (logInResponse.message == MESSAGE_SUCCESS)
+	{
+		return MESSAGE_SUCCESS;
+	}
+	
 }
 
+
+
 /*
-* Show posting UI
+* Show list event UI
 * param:
 *  client[IN] - socket param to pass to startComunicatingWithServer function
 * return feature code
 */
-int showPostFeature(SOCKET& client)
+int showListEvent(SOCKET& client)
 {
-	cout << "Leave your post empty to exit the posting feature." << endl;
-	while (1)
-	{
-		string input = getUserInput("Enter your post: ");
-		if (input == "\r\n")
-			break;
-		vector<string> requestParts = { CMD_POST, input };
-		string request = joinToString(requestParts);
-		int responseCode = startComunicatingWithServer(client, request.c_str());
-		handleResponseCode(responseCode);
-	}
-	return 2;
+	cout << "All events" << endl;
+	ListEventRequest listEventRequest;
+	string rawRequest = listEventRequest.serialize();
+	string response = startComunicatingWithServer(client, rawRequest.c_str());
+	ListEventResponse listEventResponse;
+	listEventResponse.deserialize(response);
+	list<Event*> events = listEventResponse.events;
+	for (Event* event : events)
+		cout << event->id << endl;
+	return 3;
+}
+
+void showlistEvent(list<Event*> events)
+{
+	for (Event* event: events)
+        cout << event->id << endl;
 }
 
 /*
@@ -202,14 +248,14 @@ int showPostFeature(SOCKET& client)
 *  client[IN] - socket param to pass to startComunicatingWithServer function
 * return feature code
 */
-int justLogout(SOCKET& client)
-{
-	vector<string> requestParts = { CMD_OUT, "\r\n" };
-	string request = joinToString(requestParts);
-	int responseCode = startComunicatingWithServer(client, request.c_str());
-	handleResponseCode(responseCode);
-	return 3;
-}
+//int justLogout(SOCKET& client)
+//{
+//	vector<string> requestParts = { CMD_OUT, "\r\n" };
+//	string request = joinToString(requestParts);
+//	int responseCode = startComunicatingWithServer(client, request.c_str());
+//	handleResponseCode(responseCode);
+//	return 3;
+//}
 /*
 * Show feature menu UI along with feature code
 * param:
@@ -219,23 +265,35 @@ int justLogout(SOCKET& client)
 int showFeaturesMenu(SOCKET& client)
 {
 	cout << "Select a feature below:" << endl;
-	cout << "1. Login" << endl;
-	cout << "2. Post a article" << endl;
-	cout << "3. Logout" << endl;
-	cout << "4. Exit" << endl;
-	cout << "Select number: ";
+	cout << "1. Register" << endl;
+	cout << "2. Login" << endl;
+	cout << "3. List Event" << endl;
+	cout << "4. List Request" << endl;
+	cout << "5. Logout";
 	string option;
 	getline(cin, option);
 	switch (atoi(option.c_str()))
 	{
 	case 1:
-		return showLoginFeature(client);
+		if(showRegisterFeature(client) == MESSAGE_SUCCESS){
+			cout << "---------  You have successfully registered ----------" << endl;
+		} else if (showRegisterFeature(client) ==  MESSAGE_NAME_INVALID)
+		{
+			cout << "----- Your account already exists -----" << endl;
+		}
+		return 1;
 		break;
 	case 2:
-		return showPostFeature(client);
+		if(showLoginFeature(client) == MESSAGE_SUCCESS){
+			cout << "--------- Successful login ----------" << endl;
+		} else if ((showLoginFeature(client) ==  MESSAGE_NAME_INVALID) || (showLoginFeature(client) ==  MESSAGE_CREDENTIAL_INVALID))
+		{
+			cout << "--------- Account or password is incorrect ----------" << endl;
+		}
+		return 2;
 		break;
 	case 3:
-		return justLogout(client);
+		return showListEvent(client);
 		break;
 	case 4:
 		return 4;
